44ef0889defad002807376c7bea017a8fa96857c
ww-side <daniil.metelia@gmail.com>
2024-06-10 22:45:41 +0300

    chore: added types

---
+++ src/types/person.ts
@@ -0,0 +1,32 @@
+export type PersonType = {
+  id: number;
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: number;
+  films: number[];
+  species: number[];
+  vehicles: number[];
+  starships: number[];
+  created: string;
+  edited: string;
+  url: string;
+};
+
+export type PersonCharacteristicsType = {
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+};
+
+export type PersonInformationType = {
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+};
---
+++ src/types/starship.ts
@@ -0,0 +1,21 @@
+export type StarshipType = {
+  id: number;
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  max_atmosphering_speed: string;
+  crew: string;
+  passengers: string;
+  cargo_capacity: string;
+  consumables: string;
+  hyperdrive_rating: string;
+  MGLT: string;
+  starship_class: string;
+  pilots: any[];
+  films: number[];
+  created: string;
+  edited: string;
+  url: string;
+};
